a6f675da00105cdc4713b1bd0cedcd6bb6f25234
leeheeae <chromeee@daum.net>
2022-09-05 17:24:53 +0900

    posts ui

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {store} from './store';
-import TodoApp from './components/TodoApp';
+// import TodoApp from './components/TodoApp';
+import PostsApp from './components/PostsApp';
 
 function App() {
   return (
     <Provider store={store}>
-      <TodoApp />
+      <PostsApp />
     </Provider>
   );
 }
--- slices/index.ts
+++ slices/index.ts
@@ -1,8 +1,9 @@
 import {combineReducers} from 'redux';
 import auth from './auth';
 import todos from './todos';
+import posts from './posts';
 
-const rootReducer = combineReducers({auth, todos});
+const rootReducer = combineReducers({auth, todos, posts});
 
 // rootReducer 함수의 반환값 타입을 RootState type alias로 지정
 export type RootState = ReturnType<typeof rootReducer>;
