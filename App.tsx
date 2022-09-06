1ce071b1becf53cf3adca5d9dae2ec97409eaf76
leeheeae <chromeee@daum.net>
2022-09-06 16:05:49 +0900

    posts recoil

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {RecoilRoot} from 'recoil';
-import AuthApp from './components/AuthApp';
+// import AuthApp from './components/AuthApp';
 // import {Provider} from 'react-redux';
 // import {store} from './store';
-import TodoApp from './components/TodoApp';
-// import PostsApp from './components/PostsApp';
+// import TodoApp from './components/TodoApp';
+import PostsApp from './components/PostsApp';
 
 function App() {
   return (
     <RecoilRoot>
-      <TodoApp />
+      <PostsApp />
     </RecoilRoot>
   );
 }
--- hooks/useTodosAction.ts
+++ hooks/useTodosAction.ts
@@ -1,15 +1,21 @@
 import {useMemo} from 'react';
-import {useRecoilValue, useSetRecoilState} from 'recoil';
+import {useRecoilCallback, useSetRecoilState} from 'recoil';
 import {nextTodoId, todosState} from '../atoms/todos';
 
 export default function useTodosActions() {
   const set = useSetRecoilState(todosState);
-  const nextId = useRecoilValue(nextTodoId);
+  const add = useRecoilCallback(
+    ({snapshot}) =>
+      async (text: string) => {
+        const nextId = await snapshot.getPromise(nextTodoId);
+        set(prevState => prevState.concat({id: nextId, text, done: false}));
+      },
+    [],
+  );
 
   return useMemo(
     () => ({
-      add: (text: string) =>
-        set(prevState => prevState.concat({id: nextId, text, done: false})),
+      add,
       remove: (id: number) =>
         set(prevState => prevState.filter(todo => todo.id !== id)),
       toggle: (id: number) =>
@@ -19,7 +25,7 @@ export default function useTodosActions() {
           ),
         ),
     }),
-    [set, nextId],
+    [set, add],
   );
 }
 
